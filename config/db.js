6198a0cc74375556945c75bd436f2ca29272a2cd
Abijeeth Vasra <abijeeth.vasra@bounteous.com>
2024-06-10 11:59:32 +0530

    aivendb

--- config/db.js
+++ config/db.js
@@ -1,12 +1,11 @@
 const mysql = require('mysql2');
-require('dotenv').config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT 
+  host: 'mydatabase-vasraabijeeth-7158.i.aivencloud.com',
+  user: 'avnadmin',
+  password: 'AVNS_4wGVzfS-mddMZbuwx9i',
+  database: 'defaultdb',
+  port: 24584
 });
 
 db.connect(err => {
