7110ab9d8b75e7aacef37235d1b774c92b26fb5f
Abijeeth Vasra <abijeeth.vasra@bounteous.com>
2024-06-10 17:13:43 +0530

    cors fix

--- index.js
+++ index.js
@@ -9,8 +9,17 @@ require('dotenv').config();
 
 const app = express();
 
+const allowedOrigins = ['https://movie-lemon-one.vercel.app', 'http://localhost:5173'];
+
 app.use(cors({
-  origin: 'https://movie-lemon-one.vercel.app' 
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
 }));
 
 app.use(express.json());
